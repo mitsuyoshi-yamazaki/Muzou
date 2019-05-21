ebd3b6337469526c021a36ec9bc2b8432b9c33fe
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2019-05-21 13:56:10 +0900

    Add Armadillidiidae Project

---
+++ Armadillidiidae/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Memo
+// https://qiita.com/4_mio_11/items/e7b0a5e65c89ac9d6d7f#こんな時はどうすれば
+// https://www.crossroad-tech.com/entry/Unity_VisualStudioCode4#２The-NET-CLI-Tools-cannot-be-located-が出る
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Vector2 start = transform.position;
+
+        Vector2 end = start + new Vector2(1, 0);
+
+        transform.position = end;
+    }
+}
